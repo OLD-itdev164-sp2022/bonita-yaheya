2741b9b7e16f47d68a871d6e5c7c89e331cea45b
BYaheya <89624914+BYaheya@users.noreply.github.com>
2022-02-13 21:25:43 -0600

    Updated Index.js to reflect image/excerpt

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,32 +1,62 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { graphql, Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      loading="eager"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
+    <ul>
+      {data.allContentfulBlogPost.edges.map(edge => (
+        <li>
+          <Link to={edge.node.slug} key={edge.node.id}>
+            {edge.node.title} </Link>
+            <div>
+              <GatsbyImage
+              image={edge.node.heroImageimage.gatsbyImageData}
+              alt="test" />
+            </div>
+            <div>
+              {edge.node.body.childMarkdownRemark.excerpt}
+            </div>
+        </li>
+
+      ))
+      }
+      </ul>
+    </Layout>
 )
+
+export default IndexPage
+
+export const query = graphql`
+{
+  allContentfulBlogPost {
+    edges {
+      node {
+        id
+        title
+        slug
+        body {
+          childMarkdownRemark {
+            excerpt
+          }
+        }
+        heroImage {
+          gatsbyImageData(
+            layout: CONSTRAINED
+            placeholder: BLURRED
+            width: 300
+          )
+        }
+      }
+    }
+  }
+}
+`
+    
+   
 
 export default IndexPage
