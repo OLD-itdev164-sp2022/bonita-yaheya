04cb86dbd4c8c5188fd13e7d7716f3d0aabf354d
BYaheya <89624914+BYaheya@users.noreply.github.com>
2022-02-26 17:28:00 -0600

    Installed gatsby-styled-components

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
 
     {
       resolve: `gatsby-source-contentful`,
